5793cd830a1f0d8cfdea3d6f7109538681a49e46
Lucas Gelfond <lucas_gelfond@brown.edu>
2023-10-29 14:32:09 -0400

    templating, doc reset work

--- server/src/main.rs
+++ server/src/main.rs
@@ -28,11 +28,31 @@ async fn main() -> mongododm::mongo::error::Result<()> {
     let db = client.database("test1");
 
 
+    // enforces that the repository i.e. collection is of type User and type Post
+    let user_repository = db.repository::<user_schema::User>();
+    let post_repository = db.repository::<post_schema::Post>();    
+
+    // Clean users and posts repositories to begin
+    user_repository.delete_many(doc! {}, None).await?;
+    post_repository.delete_many(doc! {}, None).await?;
+
+    // Show we have empty repositories
+    println!("Repository start state (no text if empty):");
+    let mut user_cursor = user_repository.find(None, None).await?;
+    while user_cursor.advance().await? {
+        println!("{:?}", user_cursor.deserialize_current()?);
+    }
+    let mut post_cusor = post_repository.find(None, None).await?;
+    while post_cusor.advance().await? {
+        println!("{:?}", post_cusor.deserialize_current()?);
+    }
+
+
     let test_user_a = User {
         username: "alice".to_string(),
         first_name: "Alice".to_string(),
         last_name: "of Wonderland".to_string(),
-        age: 2,
+        age: 0,
         email: "alice_of_wonderland@brown.edu".to_string(),
     };
 
@@ -40,7 +60,7 @@ async fn main() -> mongododm::mongo::error::Result<()> {
         username: "bob".to_string(),
         first_name: "Bob".to_string(),
         last_name: "Ross".to_string(),
-        age: 75,
+        age: 0,
         email: "bob_ross@brown.edu".to_string(),
     };
 
@@ -50,36 +70,46 @@ async fn main() -> mongododm::mongo::error::Result<()> {
         date: "12th March 2023".to_string(),
     };
 
-    // enforces that the repository i.e. collection is of type User
-    let user_repository = db.repository::<user_schema::User>();
+    let post_b = Post {
+        text: "Hello this is another post".to_string(),
+        posted_by_username: test_user_a.username.to_string().clone(), 
+        date: "12th March 2023".to_string(),
+    };
+
+    let post_c = Post {
+        text: "Hello this is a third post".to_string(),
+        posted_by_username: test_user_b.username.to_string().clone(), 
+        date: "12th March 2023".to_string(),
+    };
+
+    // add users and posts 
     user_repository.insert_one(&test_user_a, None).await?;
     user_repository.insert_one(&test_user_b, None).await?;
 
-    let mut users_cursor = user_repository.find(doc! {"$and": vec! [doc! {"age": doc! {"$lt": 100} }]}, None).await?;
-    while users_cursor.advance().await? {
-        println!("{:?}", users_cursor.deserialize_current()?);
+    post_repository.insert_one(&post_a, None).await?;
+    post_repository.insert_one(&post_b, None).await?;
+    post_repository.insert_one(&post_c, None).await?;
+
+    println!("New set of users and posts:");
+    let mut user_cursor = user_repository.find(None, None).await?;
+    while user_cursor.advance().await? {
+        println!("{:?}", user_cursor.deserialize_current()?);
+    }
+    let mut post_cusor = post_repository.find(None, None).await?;
+    while post_cusor.advance().await? {
+        println!("{:?}", post_cusor.deserialize_current()?);
     }
 
-    let post_repository = db.repository::<post_schema::Post>();
-    post_repository.insert_one(&post_a, None).await?;
+
+
+    // println!("Test user a username {:?}", test_user_a.username);
+
+    // let found_post = post_repository
+    //     .find_one(doc! { f!(text in Post): "Hello this is a post" }, None)
+    //     .await?
+    //     .unwrap();
     
-
-    println!("Test user a username {:?}", test_user_a.username);
-
-    let found_user = user_repository
-        .find_one(doc! { f!(username in User): "Alice" }, None)
-        .await?
-        .unwrap();
-
-    println!("Found user: {:?}", found_user);
-
-
-    let found_post = post_repository
-        .find_one(doc! { f!(text in Post): "Hello this is a post" }, None)
-        .await?
-        .unwrap();
-    
-    println!("Found post: {:?}", found_post);
+    // println!("Found post: {:?}", found_post);
 
 
     Ok(())
